e89053895cf0ee7692a8f25afe50802449ea8712
Blake Kostner <git@btkostner.io>
2021-06-11 10:39:03 -0600

    trim down the fat and fix main component styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <sys-omnibar>
+      <template v-slot:global>
+        <a href="https://system76.com/specials">
+          Special Offers
+        </a>
+      </template>
+    </sys-omnibar>
+
     <sys-headerbar
       url-homepage="https://system76.com"
       url-laptops="https://system76.com/laptops"
@@ -29,65 +37,6 @@
       url-privacy="https://system76.com/privacy"
       url-disclaimer="https://system76.com/disclaimer"
       url-terms="https://system76.com/terms"
-      show-newsletter-button
-      @subscribe="toggleSubscribeModal"
     />
   </div>
 </template>
-
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>

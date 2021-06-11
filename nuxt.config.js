c191e45794f137098f04e19b4ff4157d217a5fc4
Blake Kostner <git@btkostner.io>
2021-06-11 10:39:03 -0600

    trim down the fat and fix main component styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,69 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  components: true,
+
   head: {
-    title: 'support',
+    description: 'Official System76 Support and useful documentation',
+    color: '#4e4540',
+
+    titleTemplate: (title) => (title !== '') ? `${title} - System76 Support` : 'System76 Support',
+
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
+      itemscope: true
     },
+
     meta: [
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge,chrome=1' },
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Official System76 Support and useful documentation' }
     ],
+
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
+
+    script: [
+      {
+        async: true,
+        defer: true,
+        'data-domain': 'support.system76.com',
+        src: 'https://plausible.io/js/plausible.js'
+      },
+      {
+        innerHTML: 'window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }'
+      }
+    ],
+
+    __dangerouslyDisableSanitizers: ['script']
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@system76/design/dist/index.common.css',
+    '@system76/components/dist/index.common.css'
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/components'
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss'
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/content
     '@nuxt/content'
   ],
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
+  loading: {
+    color: '#6ACAD8'
+  },
+
   content: {
     dir: './_jekyll/_articles',
     fullTextSearchFields: ['title', 'description']
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
   }
 }

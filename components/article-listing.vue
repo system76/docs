4b9ef52d332a740bafdddf963457bca63f8e5580
Blake Kostner <git@btkostner.io>
2021-06-13 02:15:44 -0600

    add basic search field on homepage

--- components/article-listing.vue
+++ components/article-listing.vue
@@ -1,12 +1,12 @@
 <template>
   <li class="block group">
     <nuxt-link :to="`/articles/${article.slug}`">
-      <dt class="text-lg leading-6 font-semibold text-gray-900 group-focus:underline group-hover:underline">
+      <div class="text-lg leading-6 font-semibold text-gray-900 group-focus:underline group-hover:underline">
         {{ article.title }}
-      </dt>
-      <dd class="mt-2 text-base text-gray-500">
+      </div>
+      <div class="mt-2 text-base text-gray-500">
         {{ article.description }}
-      </dd>
+      </div>
     </nuxt-link>
   </li>
 </template>

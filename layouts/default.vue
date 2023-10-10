7a35d5a55714c1b9060e9cf1ae3bf5259f85cd58
Mike Garegnani <systems.support@gmail.com>
2023-10-10 08:24:18 -0700

    remove gear up promo

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
           href="https://system76.com/specials"
           target="_blank"
         >
-          Gear Up and SAVE up to $150!
+          Special Offers
         </a>
       </template>
 

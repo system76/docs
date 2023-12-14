65c5ad8a3743490820f235106099b72a3180c5a6
Aaron Honeycutt <aaron@system76.com>
2023-12-14 10:58:04 -0700

    Adds a script to setup development for working on the docs project

---
+++ setup-development.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update system and install packages
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg
+
+# Create directory for keys and install the key there.
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+# Add repository for nodejs
+NODE_MAJOR=18
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list

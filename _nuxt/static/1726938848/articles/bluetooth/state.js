c38a124826bb1ff0a1c1ac85335e3c8809b01127
ahoneybun <ahoneybun@users.noreply.github.com>
2024-09-21 17:18:34 +0000

    deploy: 3e4f15f1e013657aa78a0dda3e090bc35ba894ea

---
+++ _nuxt/static/1726938848/articles/bluetooth/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1726938848",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fbluetooth",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f8882473"}}}}(null));

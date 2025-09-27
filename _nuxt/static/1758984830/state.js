0c681a4945e6def3bc81c01f785362fb3353afbf
WatchMkr <WatchMkr@users.noreply.github.com>
2025-09-27 14:58:19 +0000

    deploy: 67e0c1d64aa1d11c168e3d0f3463dcb80d872755

---
+++ _nuxt/static/1758984830/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1758984830",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0a54a56d"}},_img:{"/_ipx/s_1207x440/images/website/open-ticket.png":"\u002F_nuxt\u002Fimage\u002F08d001.png"}}}(null,"\u002F"));

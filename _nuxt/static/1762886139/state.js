b0ceb9a030422c43fd4e81eb18d280c38b4e51c8
thomas-zimmerman <thomas-zimmerman@users.noreply.github.com>
2025-11-11 18:40:08 +0000

    deploy: 347968895e97341f2e52c76f2f64654a900318d2

---
+++ _nuxt/static/1762886139/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1762886139",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"aad4ac26"}},_img:{"/_ipx/s_1207x440/images/website/open-ticket.png":"\u002F_nuxt\u002Fimage\u002F08d001.png"}}}(null,"\u002F"));

59dee61d1893b009882b896a458f8e1f717f117f
Socialhappiness <Socialhappiness@users.noreply.github.com>
2025-12-17 20:12:01 +0000

    deploy: a42369295ee11940b3079d76355925b4a5b437ff

---
+++ _nuxt/static/1766002047/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1766002047",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"39406490"}},_img:{"/_ipx/s_1207x440/images/website/open-ticket.png":"\u002F_nuxt\u002Fimage\u002F08d001.png"}}}(null,"\u002F"));

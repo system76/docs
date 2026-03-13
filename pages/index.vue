766bd7fcfcb8cfa0fc1650849d7d85ecff405d17
Emma Truong <emma@system76.com>
2026-03-13 13:48:08 -0600

    routing the tech docs link directly to tech docs

--- pages/index.vue
+++ pages/index.vue
@@ -116,10 +116,13 @@
 
         <ul class="space-y-2">
           <li>
-            <nuxt-link
-              to="/articles/guides"
+            <a
+              href="https://tech-docs.system76.com/"
               class="flex items-center px-4 py-3 transition duration-150 ease-in-out rounded-md group hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-orange-500"
-            >
+              target="_blank"
+              @click.stop
+              rel="noopener noreferrer"
+              >
               <div class="flex items-center justify-center flex-shrink-0 w-10 h-10 rounded-full bg-warm-gray-600 text-white sm:h-12 sm:w-12 transition duration-150 ease-in-out group-hover:bg-orange-500 group-focus:bg-orange-500">
                 <font-awesome-icon icon="screwdriver" />
               </div>
@@ -131,9 +134,8 @@
                   Diagrams and components for System76 hardware.
                 </p>
               </div>
-            </nuxt-link>
+            </a>
           </li>
-
         </ul>
       </div>
 
